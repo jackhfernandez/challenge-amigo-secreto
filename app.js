674ecb6e38146c4e999200918ed05f77808a4b88
Jack <132464970+jackhfernandez@users.noreply.github.com>
2025-08-10 23:38:56 -0500

    Agregando funcion mostrarAmigos()

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ let amigos = [];
 
 function agregarAmigo() {
   // Captura el valor del campo de entrada
-  const inputAmigo = document.getElementById('amigo');
+  const inputAmigo = document.getElementById("amigo");
   const nombreAmigo = inputAmigo.value.trim();
 
   // Validar la entrada
-  if (nombreAmigo === ''){
-    alert('Por favor, inserte un nombre valido.');
+  if (nombreAmigo === "") {
+    alert("Por favor, inserte un nombre valido.");
     return;
   }
 
@@ -15,7 +15,25 @@ function agregarAmigo() {
   amigos.push(nombreAmigo);
 
   // Limpiar el campo de entrada
-  inputAmigo = '';
+  inputAmigo.value = "";
 
-  // ..
+  // Actualizar la lista visual
+  mostrarAmigos();
+}
+
+function mostrarAmigos() {
+  // Obtener nombres de la lista
+  const lista = document.getElementById("listaAmigos");
+
+  // Limpiar la lista existente
+  lista.innerHTML = "";
+
+  // Iterar sobre el arreglo usando bucle for
+  for (let i = 0; i < amigos.length; i++) {
+    const li = document.createElement("li");
+    li.textContent = amigos[i];
+
+    // Agregar nombre a la lista
+    lista.appendChild(li);
+  }
 }
